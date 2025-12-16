fb2fda5aa37c0a35991af57bc354dea866436d36
Ken Ken <malehornk@gmail.com>
2025-12-15 19:21:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -182,5 +182,8 @@ def admin_logout():
     flash("Admin logged out!", "info")
     return redirect(url_for("home"))
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
